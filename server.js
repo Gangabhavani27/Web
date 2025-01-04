607815f90ad4a0a3ed2708e028a2bd3bee5cfbee
Gangabhavani27 <gangabhavani.palupuri@sasi.ac.in>
2025-01-04 16:02:47 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,6 @@
+// const express=require('express');
+// const app=express();
+// app.get('/',(req,res) => {
+//     res.send('<h1>Welcome</h1>');
+// })
+// app.listen(5000,()=>console.log('Server is Running'));
